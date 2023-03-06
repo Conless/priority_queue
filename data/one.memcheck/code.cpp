9475a31bf481b381bafb169779be817ff332b0a9
Conless <conlesspan@outlook.com>
2023-03-06 14:08:27 +0800

    feat(data): add the new data for DataStructure2023.

--- data/one.memcheck/code.cpp
+++ data/one.memcheck/code.cpp
@@ -51,10 +51,69 @@ void TestException()
 	}
 }
 
+struct Natural {
+    int x;
+
+    Natural(int _x = 0) { x = _x; }
+
+    friend bool operator<(const Natural &lhs, const Natural &rhs) {
+        if (lhs.x < 0 || rhs.x < 0)
+            throw sjtu::runtime_error();
+        return lhs.x < rhs.x;
+    }
+};
+
+void TestCompareException() {
+	std::cout << "Testing compare exception...";
+
+	sjtu::priority_queue<Natural> pq;
+    static int dat[2000], ans[2000];
+	int pos = 0;
+    for (int i = 1; i <= 1000; ++i) {
+        dat[i] = i;
+		if (rand() % 10 == 0) 
+			dat[i] = -i;
+		else ans[++pos] = i;
+	}
+
+    for (int i = 1000; i > 1; --i) {
+        unsigned int x = rand();
+        int p = x % (i - 1) + 1;
+        std::swap(dat[i], dat[p]);
+    }
+
+    while (dat[1] < 0) {
+        unsigned int x = rand();
+        int p = x % 1000 + 1;
+        std::swap(dat[1], dat[p]);
+    }
+
+
+    for (int i = 1; i <= 1000; ++i) {
+        try {
+            pq.push(Natural(dat[i]));
+        } catch (sjtu::runtime_error) {
+            if (dat[i] >= 0)
+                return std::cout << std::endl, void();
+        }
+    }
+
+    for (int i = pos; i; --i) {
+        Natural t = pq.top();
+        if (t.x != ans[i])
+                return std::cout << std::endl, void();
+        pq.pop();
+    }
+
+    std::cout << "ok." << std::endl;
+
+}
+
 int main(int argc, char *const argv[])
 {
 	TestConstructorAndPush();
 	TestSize();
 	TestException();
+    TestCompareException();
 	return 0;
 }
